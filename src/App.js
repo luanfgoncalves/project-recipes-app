643d98e7d89dc4074cd221204ff509682ae2ef90
Luan Filipe <101883790+luanfgoncalves@users.noreply.github.com>
2022-08-19 19:58:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
