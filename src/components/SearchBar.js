0821eb1dec875b5b0cfd86078f32eb388b90dd29
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-08-22 17:27:57 -0300

    Requisito 10 - conclusão do requisito

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,6 +9,43 @@ const SearchBar = () => (
         id="search-input"
       />
     </label>
+    <div>
+      <label htmlFor="ingredient-search-radio">
+        Ingredient
+        <input
+          type="radio"
+          id="ingredient-search-radio"
+          data-testid="ingredient-search-radio"
+          name="search-type"
+          value="ingredient"
+        />
+      </label>
+      <label htmlFor="name-search-radio">
+        Name
+        <input
+          type="radio"
+          id="name-search-radio"
+          data-testid="name-search-radio"
+          name="search-type"
+          value="name"
+        />
+      </label>
+      <label htmlFor="first-letter-search-radio">
+        First Letter
+        <input
+          type="radio"
+          id="first-letter-search-radio"
+          data-testid="first-letter-search-radio"
+          name="search-type"
+          value="first-letter"
+        />
+      </label>
+      <input
+        type="button"
+        data-testid="exec-search-btn"
+        value="Buscar"
+      />
+    </div>
   </div>
 );
 
