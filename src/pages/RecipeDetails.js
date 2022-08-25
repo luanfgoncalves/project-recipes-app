cbd84b3309a5330e4edce1611c24fe440d68010b
luanfgoncalves <luanfgoncalves@outlook.com>
2022-08-25 19:51:44 -0300

    Feat: Req 25 completo, fetchs para Meals e Drinks a partir do :id, validação com base na rota e principio do req 26

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,12 +1,11 @@
 // import React from 'react';
 // import React, { useEffect, useState } from 'react';
 import React, { useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import AppReceitasContext from '../context/AppReceitasContext';
+import { fetchDrinkApi, fetchMealApi } from '../services/fetchDrinksAndMeals';
 
 const RecipeDetails = () => {
-//   const [recipe, setRecipe] = useState([]);
-//   const [recipeType, setRecipeType] = useState('');
   const {
     recipe,
     setRecipe,
@@ -14,39 +13,40 @@ const RecipeDetails = () => {
     setRecipeType,
   } = useContext(AppReceitasContext);
 
-  // const history = useHistory();
-  // const { location: { pathname } } = history;
-  // console.log(location);
+  const history = useHistory();
+  const { location: { pathname } } = history;
   const { id } = useParams();
 
   const fetchRecipeApi = async (endpoint) => {
-    try {
-      const mealResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${endpoint}`);
-      const drinkResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${endpoint}`);
-      const mealData = await mealResponse.json();
-      console.log(mealData);
-      const drinkData = await drinkResponse.json();
+    const noNumbers = pathname.replace(/[0-9]/g, '');
+    const oito = 8;
+    console.log(noNumbers.length);
+
+    if (noNumbers.length === oito) {
+      const drinkData = await fetchDrinkApi(endpoint);
       console.log(drinkData);
-      if (mealData.meals === null) {
-        setRecipe(drinkData.drinks[0]);
-        setRecipeType('drink');
-        console.log('Foi recuperada uma bebida');
-        console.log(recipe);
-      }
-      if (drinkData.drinks === null) {
-        setRecipe(mealData.meals[0]);
-        setRecipeType('meal');
-        console.log('Foi recuperada uma comida');
-        console.log(recipe);
-      }
-    } catch (error) {
-      console.log('Não foi obtida uma resposta da API');
+      setRecipe(drinkData.drinks[0]);
+      setRecipeType('drink');
+      console.log('Foi recuperada uma bebida');
+      console.log(recipe);
+    }
+
+    if (noNumbers.length !== oito) {
+      const mealData = await fetchMealApi(endpoint);
+      console.log(mealData);
+      setRecipe(mealData.meals[0]);
+      setRecipeType('meal');
+      console.log('Foi recuperada uma comida');
+      console.log(recipe);
     }
   };
 
   useEffect(() => {
+    // pathValidation(pathStr);
     fetchRecipeApi(id);
     console.log(`A api foi chamada com o ID ${id}`);
+    console.log(`O pathname é ${pathname}`);
+    // console.log(`O pagePath é ${pagePath}`);
   }, []);
 
   const ingredients = Object.keys(recipe).filter((e) => e.includes('strIngredient'));
@@ -59,6 +59,18 @@ const RecipeDetails = () => {
         </li>
       )));
 
+  // const cardNumber = 6;
+  // const renderMealRecomendations = () => (
+  //   ata.slice(0, maxCards).map((recomendation, index) => (
+  //     <div key={ index }>
+  //       {index <= cardNumber && (
+  //         <div data-testid={ `${index}-recomendation-card` }>
+  //           <img src={ recomendation.strDrinkThumb } alt={ recomendation.strDrink } />
+  //           <p data-testid={ `${index}-recomendation-title` }>{recomendation.strDrink}</p>
+  //         </div>)}
+  //     </div>
+  //   )));
+
   const renderMeals = () => (
     <div>
       <h2 data-testid="recipe-title">{recipe.strMeal}</h2>
@@ -67,6 +79,7 @@ const RecipeDetails = () => {
       <p data-testid="instructions">{recipe.strInstructions}</p>
 
       <iframe
+        data-testid="video"
         title={ recipe.strMeal }
         width="400"
         height="315"
@@ -77,6 +90,9 @@ const RecipeDetails = () => {
       <ul>
         {renderIngredients()}
       </ul>
+
+      <div data-testid="recomendation-card">Sou uma recomendação :3</div>
+      <div data-testid="0-recomendation-card">também sou uma recomendação :3</div>
 
     </div>
   );
@@ -92,6 +108,9 @@ const RecipeDetails = () => {
       <ul>
         {renderIngredients()}
       </ul>
+
+      <div data-testid="recomendation-card">Oi, sou uma recomendação :3</div>
+      <div data-testid="0-recomendation-card">Oi, também sou uma recomendação :3</div>
 
     </div>
   );
