56075fcbe64d92ecd0782be02628c1fe136187ab
Luan Filipe <101883790+luanfgoncalves@users.noreply.github.com>
2022-08-31 10:59:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import Login from './pages/Login';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import Profile from './pages/Profile';
-
-import Recipes from './pages/Recipes';
+import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
 import RecipeDetails from './pages/RecipeDetails';
 import RecipeInProgress from './components/RecipeInProgress';
-
 import AppReceitasProvider from './context/AppReceitasProvider';
 
 function App() {
@@ -20,14 +19,12 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/" exact component={ Login } />
-          <Route exact path="/foods" component={ Recipes } />
+          <Route exact path="/foods" component={ Foods } />
           <Route exact path="/foods/:id" component={ RecipeDetails } />
           <Route exact path="/foods/:id/in-progress" component={ RecipeInProgress } />
-
-          <Route exact path="/drinks" component={ Recipes } />
+          <Route exact path="/drinks" component={ Drinks } />
           <Route exact path="/drinks/:id" component={ RecipeDetails } />
           <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/done-recipes" component={ DoneRecipes } />
           <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
