2db6cecbf145d8107d25f80b3dcdeb873c532048
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-08-31 19:59:14 -0300

    implementação dos botoes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Profile = () => (
   <div>
     <Header title="Profile" />
+    <h5 data-testid="profile-email">Email</h5>
+    <Link to="/done-recipes" data-testid="profile-done-btn">Done Recipes</Link>
+    <Link
+      to="/favorite-recipes"
+      data-testid="profile-favorite-btn"
+    >
+      Favorite Recipes
+    </Link>
+    <Link to="/" data-testid="profile-logout-btn">Logout</Link>
     <Footer />
   </div>
 );
